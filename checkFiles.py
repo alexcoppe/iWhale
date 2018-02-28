cc20e96ac7429a4f58be2b4344702eec2b96ffff
Bino <andrea.binatti86@gmail.com>
2018-02-28 16:40:18 +0100

    Finished checkFiles.py and added test directory in .gitignore

--- checkFiles.py
+++ checkFiles.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python
 
-print "ciao"
+import os
+from subprocess import call
+import argparse
+import sys
+
+def main():
+    parser = argparse.ArgumentParser(description="Check fastq files in directory")
+    parser.add_argument('-s', '--sconsdir', action='store', type=str, help="Path to directory containing SConstruct and settings.py files", default=".")
+    parser.add_argument('-p', '--pairedend', action='store', type=bool, help="Paired-end or Single-end analysis (paired-end default)", default=True)
+    args = parser.parse_args()
+    empty_directories = []
+    for f in os.listdir(os.getcwd()):
+        if os.path.isdir(f):
+            os.chdir(f)
+            if args.pairedend == True:
+                if not (os.path.isfile("1.fastq.gz") and os.path.isfile("2.fastq.gz")):
+                    empty_directories.append(f)
+                os.chdir("..")
+            else:
+                print "single"
+    if len(empty_directories) > 0:
+        nameEmpytDir = ",".join(empty_directories)
+        sys.stderr.write("{} directories are empty or don't have paired fastq files\n".format(nameEmpytDir))
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+
+
+if __name__ == "__main__":
+    main()
