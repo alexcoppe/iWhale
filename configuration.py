1062010f514bf071b3a43b3e7ac92efc569933b2
Ale <alexcoppe@gmail.com>
2018-07-05 18:28:13 +0200

    Added clinvar to configuration file

--- configuration.py
+++ configuration.py
@@ -5,18 +5,16 @@ processors = 3
 #reference = "/home/andrea/Andrea/Script/Docker/wes_analysis/test/genome/reference.fa"
 reference = "reference.fa"
 
-#gatk4 = "/home/andrea/Andrea/Script/Docker/wes_analysis/test/gatk-4.0.2.1/gatk"
 
-#mutect = "/home/andrea/Andrea/Script/Docker/wes_analysis/test/mutect-1.1.7.jar"
-
-#cosmic = "/home/andrea/Andrea/Script/Docker/wes_analysis/test/annotation/Cosmic/CosmicCodingMuts_v82.vcf"
 cosmic = "CosmicCodingMuts_v82.vcf"
 
-#dbsnpVCF = "/home/andrea/Andrea/Script/Docker/wes_analysis/test/annotation/dbSNP_hg19/dbSNP_v150_20170710_noCHR.vcf.gz"
 dbsnpVCF = "dbSNP_v150_20170710_noCHR.vcf.gz"
 
-#exomeRegions = "/home/andrea/Andrea/Script/Docker/wes_analysis/test/annotation/bed/S07604514_Padded_noChr.bed"
+# ClinVar vcf file
+clinvar = "clinvar.vcf"
+
 exomeRegions = "small.bed"
+#exomeRegions = "SureSelect_Human_All_Exon_UTR_V5.bed"
 
 gnomadVCF = "gnomAD_lite_docker.vcf"
 
