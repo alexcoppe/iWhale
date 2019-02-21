d354e1a893fe1b6890b783f7e7ea8f25a42d71d7
Ale <alexcoppe@gmail.com>
2019-02-21 09:44:41 +0100

    Fixed bug in CombineVariants step. Changed values of some SCons parameters

--- configuration.py
+++ configuration.py
@@ -1,43 +1,48 @@
-#Computational resources
-
-processors = 3
-
-#reference = "/home/andrea/Andrea/Script/Docker/wes_analysis/test/genome/reference.fa"
-reference = "reference.fa"
-
-
-cosmic = "CosmicCodingMuts_v82.vcf"
-
-dbsnpVCF = "dbSNP_v150_20170710_noCHR.vcf.gz"
-
-# ClinVar vcf file
-clinvar = "clinvar.vcf"
-
-exomeRegions = "small.bed"
-#exomeRegions = "SureSelect_Human_All_Exon_UTR_V5.bed"
-
-gnomadVCF = "gnomAD_lite_docker.vcf"
-
-contamination = "y"
-
-javaOptions = 4
-
-
-snpeffDir = "/annotations"
-snpeffGenomeVersion = "GRCh37.75"
-
-maxPopulationAlleleFrequency = 0.2
-
-minPopulationAlleleFrequency = 0
-
-#gnomadVCF = "/home/andrea/Andrea/Script/Docker/wes_analysis/test/annotation/gnomAD/gnomAD_lite_docker.vcf"
-
+# Variant callers to be used, separated by ,
 variantCallers = "mutect,mutect2,strelka2,varscan"
 
-#### MuTect2 parameters ####
+# Computational resources
+processors = 3
+
+# Human reference
+reference = "reference.fa"
+
+# Cosmic database in VCF format
+cosmic = "CosmicCodingMuts_v82.vcf"
+
+# dbSNP database in VCF format
+dbsnpVCF = "dbSNP_v150_20170710_noCHR.vcf.gz"
+
+# ClinVar in VCF format
+clinvar = "clinvar.vcf"
+
+# Exome regions in bed format
+exomeRegions = "SureSelect_Human_All_Exon_UTR_V5.bed"
+
+# Genome aggregation database gnomAD in VCF format
+gnomadVCF = "af-only-gnomad.raw.sites.b37.vcf.gz"
+
+# Computation of predicted contamination in tumor samples by CalculateContamination from GATK4 (y/n)
+contamination = "y"
+# Predicted contamination in tumor samples maxPopulationAlleleFrequency parameter
+maxPopulationAlleleFrequency = 0.2
+# Predicted contamination in tumor samples minPopulationAlleleFrequency parameter
+minPopulationAlleleFrequency = 0
+
+# Options to run Java
+javaOptions = 4
+
+# snpeff directory location
+snpeffDir = "/annotations"
+# snpeff Genome Version
+snpeffGenomeVersion = "GRCh37.75"
+
+
+#### MuTect2 parameters ##############
 mutect2Parameters = "--dont-use-soft-clipped-bases --output-mode EMIT_VARIANTS_ONLY"
 
-############## VarScan parameters ###############
+
+### VarScan parameters ###############
 # VarScan --min-coverage
 varScanMinCoverage = 10
 # VarScan --pvalue
@@ -45,8 +50,9 @@ varScanPvalue = 0.05
 # Varscan --min-var-freq 0.02 
 varScanMinVarFreq = 0.02
 
-# Custom variant callers parameters
 
+
+# Custom variant callers parameters
 mutectParameters = ""
 mutect2Parameters = ""
 varScanParameters = ""
